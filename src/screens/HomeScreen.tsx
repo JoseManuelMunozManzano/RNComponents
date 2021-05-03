bc716582a82d375fe50bb95f02743d6ae2924098
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-05-03 17:47:53 +0200

    FlatList Separator y Header

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {View, FlatList, Text} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
+
+import {styles} from '../theme/appTheme';
 
 interface MenuItem {
   icon: string;
@@ -13,6 +16,11 @@ const menuItems: MenuItem[] = [
     name: 'Animation 101',
     icon: 'cube-outline',
     components: 'Animation101Screen',
+  },
+  {
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    components: 'Animation102Screen',
   },
 ];
 
@@ -27,19 +35,31 @@ export const HomeScreen = () => {
     );
   };
 
+  const renderListHeader = () => {
+    const {top} = useSafeAreaInsets();
+
+    return (
+      <View style={{marginTop: top + 20, marginBottom: 20}}>
+        <Text style={styles.title}>Opciones de Menú</Text>
+      </View>
+    );
+  };
+
+  const itemSeparator = () => {
+    return (
+      <View style={{borderBottomWidth: 1, opacity: 0.4, marginVertical: 8}} />
+    );
+  };
+
   return (
-    <View style={styles.container}>
+    <View style={{...styles.container, ...styles.globalMargin}}>
       <FlatList
         data={menuItems}
         renderItem={({item, index}) => renderMenuItem(item)}
         keyExtractor={item => item.name}
+        ListHeaderComponent={renderListHeader}
+        ItemSeparatorComponent={itemSeparator}
       />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
