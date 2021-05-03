adb25c068e5d77bddde2c78bbdf3f829220dd53b
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-05-03 19:06:44 +0200

    FadeIn y FadeOut

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,12 +1,37 @@
 import React, {useRef} from 'react';
-import {Animated, StyleSheet, View} from 'react-native';
+import {Animated, Button, StyleSheet, View} from 'react-native';
 
 export const Animation101Screen = () => {
-  const opacity = useRef(new Animated.Value(0.4)).current;
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => {
+      console.log('Animación terminó');
+    });
+  };
+
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
 
   return (
     <View style={styles.container}>
-      <Animated.View style={{...styles.purpleBox, opacity: opacity}} />
+      <Animated.View
+        style={{...styles.purpleBox, marginBottom: 20, opacity: opacity}}
+      />
+
+      <View style={styles.buttonContainer}>
+        <Button title="FadeIn" onPress={fadeIn} />
+        <Button title="FadeOut" onPress={fadeOut} />
+      </View>
     </View>
   );
 };
@@ -21,5 +46,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#5856D6',
     width: 150,
     height: 150,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '60%',
+    justifyContent: 'space-between',
   },
 });
