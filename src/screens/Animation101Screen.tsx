8572bd703cb944cfab8d442abcf2ee15712392f5
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-05-03 19:23:49 +0200

    Easing - Bounce

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,8 +1,9 @@
 import React, {useRef} from 'react';
-import {Animated, Button, StyleSheet, View} from 'react-native';
+import {Animated, Button, Easing, StyleSheet, View} from 'react-native';
 
 export const Animation101Screen = () => {
   const opacity = useRef(new Animated.Value(0)).current;
+  const top = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = () => {
     Animated.timing(opacity, {
@@ -12,6 +13,13 @@ export const Animation101Screen = () => {
     }).start(() => {
       console.log('Animación terminó');
     });
+
+    Animated.timing(top, {
+      toValue: 0,
+      duration: 800,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start();
   };
 
   const fadeOut = () => {
@@ -19,13 +27,28 @@ export const Animation101Screen = () => {
       toValue: 0,
       duration: 300,
       useNativeDriver: true,
-    }).start();
+    }).start(() =>
+      Animated.timing(top, {
+        toValue: -100,
+        duration: 0,
+        useNativeDriver: true,
+      }).start(),
+    );
   };
 
   return (
     <View style={styles.container}>
       <Animated.View
-        style={{...styles.purpleBox, marginBottom: 20, opacity: opacity}}
+        style={{
+          ...styles.purpleBox,
+          marginBottom: 20,
+          opacity,
+          transform: [
+            {
+              translateY: top,
+            },
+          ],
+        }}
       />
 
       <View style={styles.buttonContainer}>
