ec0ac6aff54871f05edebf71f4da184e466862eb
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-05-03 08:17:53 +0200

    Componente - FlatList

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,45 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {View, StyleSheet, FlatList, Text} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+interface MenuItem {
+  icon: string;
+  name: string;
+  components: string;
+}
+
+const menuItems: MenuItem[] = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    components: 'Animation101Screen',
+  },
+];
+
 export const HomeScreen = () => {
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text>
+          {menuItem.name} - {menuItem.icon}
+        </Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Icon name="star-outline" size={50} color="black" />
+    <View style={styles.container}>
+      <FlatList
+        data={menuItems}
+        renderItem={({item, index}) => renderMenuItem(item)}
+        keyExtractor={item => item.name}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
