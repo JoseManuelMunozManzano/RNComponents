268801dd7f395942b17293040ffb42b2a1da2169
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-05-12 21:19:08 +0200

    Componente - SectionList

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -8,6 +8,7 @@ import {SwitchScreen} from '../screens/SwitchScreen';
 import {AlertScreen} from '../screens/AlertScreen';
 import {TextInputScreen} from '../screens/TextInputScreen';
 import {PullToRefreshScreen} from '../screens/PullToRefreshScreen';
+import {CustomSectionListScreen} from '../screens/CustomSectionListScreen';
 
 export type RootStackParams = {
   HomeScreen: undefined;
@@ -17,6 +18,7 @@ export type RootStackParams = {
   AlertScreen: undefined;
   TextInputScreen: undefined;
   PullToRefreshScreen: undefined;
+  CustomSectionListScreen: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParams>();
@@ -36,6 +38,10 @@ export const Navigation = () => {
       <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
       <Stack.Screen name="AlertScreen" component={AlertScreen} />
       <Stack.Screen name="TextInputScreen" component={TextInputScreen} />
+      <Stack.Screen
+        name="CustomSectionListScreen"
+        component={CustomSectionListScreen}
+      />
       <Stack.Screen
         name="PullToRefreshScreen"
         component={PullToRefreshScreen}
