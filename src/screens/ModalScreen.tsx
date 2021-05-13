460a6127fc2590b880db135e179e73ed46c83430
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2021-05-13 20:28:51 +0200

    Personalizar el modal

--- src/screens/ModalScreen.tsx
+++ src/screens/ModalScreen.tsx
@@ -17,14 +17,31 @@ export const ModalScreen = () => {
         {/* Background */}
         <View
           style={{
-            // height: 100,
             flex: 1,
             backgroundColor: 'rgba(0,0,0,0.3)',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}>
           {/* Contenido del modal */}
-          <View style={{backgroundColor: 'white'}}>
-            <HeaderTitle title="Modal" />
-            <Text>Cuerpo del modal</Text>
+          <View
+            style={{
+              backgroundColor: 'white',
+              width: 200,
+              height: 200,
+              justifyContent: 'center',
+              alignItems: 'center',
+              shadowOffset: {
+                width: 0,
+                height: 10,
+              },
+              shadowOpacity: 0.25,
+              elevation: 10,
+              borderRadius: 5,
+            }}>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Modal</Text>
+            <Text style={{fontSize: 16, fontWeight: '300', marginBottom: 20}}>
+              Cuerpo del modal
+            </Text>
             <Button title="Cerrar" onPress={() => setIsVisible(false)} />
           </View>
         </View>
